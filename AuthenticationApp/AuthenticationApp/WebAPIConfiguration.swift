c77c8e9965efb8cffe1dd3a0cc630c50d2587180
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-10-15 00:49:28 +0200

    Added the web API configuration file.

---
+++ AuthenticationApp/AuthenticationApp/WebAPIConfiguration.swift
@@ -0,0 +1,13 @@
+//
+//  WebAPIConfiguration.swift
+//  AuthenticationApp
+//
+//  Created by Ahmed Elbasha on 10/15/18.
+//  Copyright © 2018 Ahmed Elbasha. All rights reserved.
+//
+
+import Foundation
+
+func generateGetAllCountriesWebApi() -> String {
+    return "https://restcountries.eu/rest/v2/all"
+}
