3ef4d99328f6f142bd5d3df9d10bd9df46d87e38
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-10-15 13:01:36 +0200

    Optimized the visualization of LoginViewController UI Design

--- AuthenticationApp/AuthenticationApp/LoginViewController.swift
+++ AuthenticationApp/AuthenticationApp/LoginViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Optimize loginButton style.
+        loginButton.backgroundColor = UIColor.white
+        loginButton.layer.cornerRadius = 5
+        loginButton.layer.borderWidth = 1
+        loginButton.layer.borderColor = UIColor.black.cgColor
+        
+        // Optimize registerButton style.
+        registerButton.backgroundColor = UIColor.white
+        registerButton.layer.cornerRadius = 5
+        registerButton.layer.borderWidth = 1
+        registerButton.layer.borderColor = UIColor.black.cgColor
     }
 
-
+    @IBAction func signInButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func registerButtonPressed(_ sender: Any) {
+    }
 }
 
